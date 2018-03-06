d4518f6dac3b91e3f2a12330a6fe62e93925b45d
Yigong <yigongh@gmail.com>
2018-03-05 20:41:59 -0500

    add a longhold bug app which the user can define the holding time

---
+++ app/src/main/java/com/example/yigonghu/leaseos_testapp/LongHoldingService.java
@@ -0,0 +1,113 @@
+package com.example.yigonghu.leaseos_testapp;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.PowerManager;
+import android.util.Log;
+
+import org.w3c.dom.ProcessingInstruction;
+
+public class LongHoldingService extends Service {
+    private final static int MINUTE_PER_MILLISECOND = 60*1000;
+    private final static String TAG = "LongHoldingService";
+    private final static long HOLDING_RATE = 1 * MINUTE_PER_MILLISECOND;
+    public final static String ACTION_PREFIX = "edu.jhu.order.leaseos_testapp.action";
+    private final static String WAKELOCK_HOLDING_STATS = ACTION_PREFIX + ".HOLDING_STATS";
+    private final static int MSG_HOLDING_STATS = 1;
+    private long mHoldingTime = 0;
+
+    @Override
+    public void onCreate() {
+        Log.d(TAG, "Starting service...");
+        IntentFilter ifilter = new IntentFilter();
+        ifilter.addAction(WAKELOCK_HOLDING_STATS);
+        registerReceiver(mActionReceiver, ifilter);
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (intent != null) {
+            Log.d(TAG, "The intent is not null");
+            String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+            mHoldingTime = Integer.parseInt(message) * MINUTE_PER_MILLISECOND;
+        } else {
+            Log.d(TAG, "The intent is null");
+            mHoldingTime = 1 * MINUTE_PER_MILLISECOND;
+        }
+
+        schedulAlarm();
+
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(TAG, "Stoping service...");
+        unregisterReceiver(mActionReceiver);
+        cancelAlarm();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    private Runnable mHoldingWakelock = new Runnable() {
+        @Override
+        public void run() {
+            Log.d(TAG, "The holding time is " + mHoldingTime + "ms");
+            PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+            PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "LeaseOS_testapp");
+            wl.acquire(mHoldingTime);
+        }
+    };
+
+    private void schedulAlarm() {
+        AlarmManager am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        PendingIntent operation = PendingIntent.getBroadcast(this, 0,
+                new Intent(WAKELOCK_HOLDING_STATS), 0);
+        am.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(),
+                HOLDING_RATE + mHoldingTime, operation);
+    }
+
+    private void cancelAlarm() {
+        AlarmManager am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        PendingIntent operation = PendingIntent.getBroadcast(this, 0,
+                new Intent(WAKELOCK_HOLDING_STATS), 0);
+        am.cancel(operation);
+    }
+
+    private final BroadcastReceiver mActionReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String actionStr = intent.getAction();
+            if (actionStr.equalsIgnoreCase(WAKELOCK_HOLDING_STATS)) {
+                mHandler.sendEmptyMessage(MSG_HOLDING_STATS);
+            }
+        }
+    };
+
+    private final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_HOLDING_STATS:
+                    Thread t = new Thread(mHoldingWakelock);
+                    t.start();
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+
+    };
+}
---
+++ app/src/main/java/com/example/yigonghu/leaseos_testapp/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.yigonghu.leaseos_testapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+    private TextView mTextMessage;
+    private TextView mStatusView = null;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mStatusView = (TextView) findViewById(R.id.holding_time);
+        mStatusView.setText("Wait for command...");
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendHoldingTime(View view) {
+        Intent intent = new Intent(this, LongHoldingService.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        mStatusView.setText("Holding time is " + Integer.parseInt(message) + " min");
+        startService(intent);
+    }
+
+
+}
